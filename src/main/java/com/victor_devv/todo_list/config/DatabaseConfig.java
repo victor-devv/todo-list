62c55057379e8ef94f3a46fe2ac0b7329c22e9a6
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-04-06 15:57:43 +0100

    chore: adds db config

---
+++ src/main/java/com/victor_devv/todo_list/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.victor_devv.todo_list.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * Database configuration for the application.
+ * Configures the data source, transaction management, and JPA repositories.
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.victor_devv.todo_list.repository")
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource(@Qualifier("dataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+}
---
+++ src/main/java/com/victor_devv/todo_list/util/Constants.java
@@ -0,0 +1,12 @@
+package com.victor_devv.todo_list.util;
+
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    // Pagination
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final int MAX_PAGE_SIZE = 100;
+}
